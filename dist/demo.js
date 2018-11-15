83ca3a667a2b70c24983ff5952a65abf99d8fcef
Alex Muench <ammuench@gmail.com>
2018-11-14 18:22:05 -0600

    Cleanup package code, remove dist in main repo, remove unneeded tsconfig bits, commit package-lock

--- dist/demo.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("./index");
-const deck = index_1.decodeDeck('ADCJQUQI30zuwEYg2ABeF1Bu94BmWIBTEkLtAKlAZakAYmHh0JsdWUvUmVkIEV4YW1wbGU_');
-console.log(deck);
-console.log(deck.cards.length);
--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { ArtifactCard, CardPreflight, CardSet, CardSetResponse, ImageObj, Reference, TextObj } from './modules/cards';
-import { ArtifactDeck, ArtifactDeckDecoder, DeckCard, DeckHero } from './modules/decks';
-export { ArtifactCard, ArtifactDeck, CardPreflight, CardSet, CardSetResponse, DeckCard, DeckHero, ImageObj, Reference, TextObj };
-export { ArtifactDeckDecoder };
-export declare const decodeDeck: (deckId: string) => ArtifactDeck;
-export declare const getSet: (setId: string) => Promise<CardSetResponse>;
