e2b979b3ee0e5230e91bac5d3471f0be0444d062
Bartek <artbartepll@gmail.com>
2018-11-25 23:02:19 +0100

    add docs, replace returning false to throwing errors

--- src/modules/encoder.ts
+++ src/modules/encoder.ts
@@ -1,52 +1,52 @@
-import { ArtifactDeck, Card } from './decks'
-/**
- * @todo DOCUMENT ALL THE FUNCTIONS
- * @todo REWRITE PORTED FUNCTION ?
- * @todo ATTACH ArtifactDeckEncoder TO DeckApi CLASS
- */
+import { ArtifactDeck, Card} from './decks'
+
 export class ArtifactDeckEncoder {
-    knHeaderSize: number;
-    version: number;
-    EncodedPrefix: string;
-    MaxBytesForVarUint32: number;
-    // version: 2
-    // EncodedPrefix: "ADC"
-    // MaxBytesForVarUint32: 5;
-    // knHeaderSize: 3
+    private version=  2
+    private EncodedPrefix=  "ADC"
+    private MaxBytesForVarUint32 =  5; // why it is there? idk
+    private knHeaderSize=  3
+
     constructor() {
-        this.version=  2
-        this.EncodedPrefix=  "ADC"
-        this.MaxBytesForVarUint32=  5;
-        this.knHeaderSize=  3
+
     }
-    //expects array("heroes" => array(id, turn), "cards" => array(id, count), "name" => name)
-    //	signature cards for heroes SHOULD NOT be included in "cards"
-    encodeDeck(deckContents: ArtifactDeck) {
+    /**
+     * Exprects AritfactDeck object returns a Artifact deck code
+     * Signature cards for heroes SHOULD NOT be included in "cards"
+     * @param { ArtifactDeck } deckContents Artifact deck object
+     * @returns {string} Returns artifact deck code as a string
+     */
+    public encodeDeck(deckContents: ArtifactDeck) {
         if (!deckContents) {
-            return false
+            throw new Error("Error encoding deck")
         }
         const bytes = this.encodeBytes(deckContents)
         if (!bytes) {
-            return false
+            throw new Error("Error encoding deck")
         }
         const deck_code = this.encodeBytesToString(bytes);
         return deck_code;
     }
-
-    encodeBytes(deckContents: ArtifactDeck) {
-        if (!deckContents['heroes'] || !deckContents['cards']) return false;
+    /**
+     *  Converts AritfactDeck object into array of bytes
+     * @param { ArtifactDeck } deckContents Artifact deck object
+     * @returns {Array<Number>} Array of bytes when error occured
+     */
+    private encodeBytes(deckContents: ArtifactDeck) {
+        
+        if (!deckContents['heroes'] || !deckContents['cards']) throw new Error("Error encoding deck");
         this.sortDeckByID(deckContents)
+        
         const countHeroes = deckContents.heroes.length
         const allCards = [...deckContents['heroes'], ...deckContents['cards']]
 
         let bytes: Array<number> = [];
         const version = this.version << 4 | this.extractNBitsWithCarry(countHeroes, 3)
-        if (!this.addByte(bytes, version)) return false;
+        if (!this.addByte(bytes, version)) throw new Error("Error encoding deck");
 
         //the checksum which will be updated at the end
         let nDummyChecksum = 0;
         const nChecksumByte = bytes.length;
-        if (!this.addByte(bytes, nDummyChecksum)) return false;
+        if (!this.addByte(bytes, nDummyChecksum)) throw new Error("Error encoding deck");
 
         // write the name size
         let nameLen = 0;
@@ -58,22 +58,22 @@ export class ArtifactDeckEncoder {
                 let amountToTrim = Math.floor((trimLen - 63) / 4);
                 amountToTrim = (amountToTrim > 1) ? amountToTrim : 1;
                 name = name.substring(0, name.length - amountToTrim);
-                trimLen = name.length; // Is it necesserry?
+                trimLen = name.length;
             }
             nameLen = name.length;
         }
 
-        if (!this.addByte(bytes, nameLen)) return false;
-        if (!this.addRemainingNumberToBuffer(countHeroes, 3, bytes)) return false;
+        if (!this.addByte(bytes, nameLen)) throw new Error("Error encoding deck");
+        if (!this.addRemainingNumberToBuffer(countHeroes, 3, bytes)) throw new Error("Error encoding deck");
 
         let prevCardId = 0;
         for (let unCurrHero = 0; unCurrHero < countHeroes; unCurrHero++) {
             // type any - check if there can be a class a DeckHero or something
             let card: any = allCards[unCurrHero];
             if (card['turn'] == 0)
-                return false;
+                throw new Error("Error encoding deck");
             if (!this.addCardToBuffer(card['turn'], card['id'] - prevCardId, bytes))
-                return false;
+                throw new Error("Error encoding deck");
             prevCardId = card['id'];
         }
 		//reset our card offset
@@ -84,12 +84,12 @@ export class ArtifactDeckEncoder {
 			//see how many cards we can group together
 			let card:any = allCards[nCurrCard];
 			if( card['count'] == 0 )
-				return false;
+				throw new Error("Error encoding deck");
 			if( card['id'] <= 0 )
-				return false;
+				throw new Error("Error encoding deck");
 			//record this set of cards, and advance
 			if( !this.addCardToBuffer( card['count'], card['id'] - prevCardId, bytes ) )
-				return false;
+				throw new Error("Error encoding deck");
 			prevCardId = card['id'];
 		}
         // save off the pre string bytes for the checksum
@@ -98,7 +98,7 @@ export class ArtifactDeckEncoder {
         {
             const nameBytes = this.unpack(name);
             for (let nameByte of nameBytes) {
-                if (!this.addByte(bytes, nameByte)) return false;
+                if (!this.addByte(bytes, nameByte)) throw new Error("Error encoding deck");
             }
         }
         let unFullChecksum = this.computeChecksum(bytes, preStringByteCount - this.knHeaderSize);
@@ -106,22 +106,20 @@ export class ArtifactDeckEncoder {
         bytes[nChecksumByte] = unSmallChecksum;
         return bytes;
     }
-    encodeBytesToString(bytes) {
+    private encodeBytesToString(bytes:Array<number>) {
         let byteCount = bytes.length;
         //if we have an empty buffer, just return
-        if (byteCount == 0) return false;
-        // const packed = this.pack(bytes);
-        // const encoded = this.base64_encode(packed);
+        if (byteCount == 0) throw new Error("Error encoding deck");
         const packedAndEncoded = this.packAndEncode(bytes)
         let deck_string = this.EncodedPrefix + packedAndEncoded;
 
-        deck_string.replace('/', '-')
-        deck_string.replace('=', '_')
+        deck_string = deck_string.replace(/\//g, '-')
+        deck_string = deck_string.replace(/=/g, '_')
         return deck_string;
     }
 
 
-     sortDeckByID(deck: ArtifactDeck): void {
+    private sortDeckByID(deck: ArtifactDeck): void {
         function sortByID(a: Card, b: Card) {
             return a.id <= b.id ? -1 : 1
         }
@@ -130,7 +128,7 @@ export class ArtifactDeckEncoder {
         deck['cards'].sort(sortByID)
     }
     
-     extractNBitsWithCarry(value: number, numBits: number) {
+    private extractNBitsWithCarry(value: number, numBits: number) {
         const unLimitBit = 1 << numBits;
         let unResult = (value & (unLimitBit - 1));
         if (value >= unLimitBit) {
@@ -139,29 +137,29 @@ export class ArtifactDeckEncoder {
         return unResult;
     }
     
-     addByte(bytes: Array<number>, byte: number) {
+    private addByte(bytes: Array<number>, byte: number) {
         if (byte > 255)
-            return false;
+            throw new Error("Error encoding deck");
         bytes.push(byte);
         return true;
     }
     
-     addRemainingNumberToBuffer(unValue: number, unAlreadyWrittenBits: number, bytes: Array<number>) {
+    private addRemainingNumberToBuffer(unValue: number, unAlreadyWrittenBits: number, bytes: Array<number>) {
         unValue >>= unAlreadyWrittenBits;
         let unNumBytes = 0;
         while (unValue > 0) {
             const unNextByte = this.extractNBitsWithCarry(unValue, 7);
             unValue >>= 7;
             if (!this.addByte(bytes, unNextByte))
-                return false;
+                throw new Error("Error encoding deck");
             unNumBytes++;
         }
         return true;
     }
     
-     addCardToBuffer(unCount, unValue, bytes: Array<number>) {
+    private addCardToBuffer(unCount:number, unValue:number, bytes: Array<number>) {
         //this shouldn't ever be the case
-        if (unCount == 0) return false;
+        if (unCount == 0) throw new Error("Error encoding deck");
         let countBytesStart = bytes.length;
         //determine our count. We can only store 2 bits, and we know the value is at least one, so we can encode values 1-5. However, we set both bits to indicate an 
         //extended count encoding
@@ -172,51 +170,40 @@ export class ArtifactDeckEncoder {
         let unFirstByte = (unFirstByteCount << 6);
         unFirstByte |= this.extractNBitsWithCarry(unValue, 5);
         if (!this.addByte(bytes, unFirstByte))
-            return false;
+            throw new Error("Error encoding deck");
     
         //now continue writing out the rest of the number with a carry flag
         if (!this.addRemainingNumberToBuffer(unValue, 5, bytes))
-            return false;
+            throw new Error("Error encoding deck");
         //now if we overflowed on the count, encode the remaining count
         if (bExtendedCount) {
             if (!this.addRemainingNumberToBuffer(unCount, 0, bytes))
-                return false;
+                throw new Error("Error encoding deck");
         }
         let countBytesEnd = bytes.length;
         if (countBytesEnd - countBytesStart > 11) {
             //something went horribly wrong
-            return false;
+            throw new Error("Error encoding deck");
         }
         return true;
     }
     
-     unpack(str: String) {
+    private unpack(str: String) {
         const binArr = []
         for (let i = 0; i < str.length; i++) {
             binArr.push(str.charCodeAt(i))
         }
         return binArr
     }
-     pack(binArr: Array<number>) {
-        const strArr = []
-        for (let byte of binArr) {
-            strArr.push(String.fromCharCode(byte))
-        }
-        return strArr.join('')
-    }
-     base64_encode(string) {
-        const buff = new Buffer(string)
-        const base64 = buff.toString('base64')
-        return base64
-    }
 
-    packAndEncode(binArr) {
+
+    private packAndEncode(binArr:Array<number>) {
             const buff = Buffer.from(binArr)
             const encoded = buff.toString('base64')
             return encoded
     }
     
-     computeChecksum(bytes, unNumBytes) {
+    private computeChecksum(bytes:Array<number>, unNumBytes:number) {
         let unChecksum = 0;
         for (let unAddCheck = this.knHeaderSize; unAddCheck < unNumBytes + this.knHeaderSize; unAddCheck++) {
             let byte = bytes[unAddCheck];
@@ -224,7 +211,6 @@ export class ArtifactDeckEncoder {
         }
         return unChecksum;
     }
-    
     
 
 }
